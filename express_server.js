80ba53d0932a5401396d761373f2a087998f8791
Eric Ho <endonoho@gmail.com>
2020-06-08 16:02:01 -0700

    Update routes

--- express_server.js
+++ express_server.js
@@ -1,23 +1,25 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const PORT = 8080;
 
-const urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
-};
+app.set('view engine', 'ejs');
+app.use(morgan('dev'));
 
-app.get("/", (req, res) => {
-    res.send("Hello!");
+
+// login page
+app.get('/', (req, res) => {
+    const name = 'John Doe';
+
+    res.render('pages/login', {
+        name: name
+    });
 });
 
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// register page
+app.get('/register', (req, res) => {
+    res.render('pages/register');
+})
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
