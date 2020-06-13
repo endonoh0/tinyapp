de74374f42e55eab5d1db5fa015a34ac075083c8
Eric Ho <endonoho@gmail.com>
2020-06-13 15:57:14 -0700

    Update url history whenever a link is shared

--- routes/shareUrls.js
+++ routes/shareUrls.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const app = module.exports = express();
 const { urlDatabase } = require('../database');
+const { generateRandomString } = require('../helpers');
+
 
 /**
  * Redirect to the specified resource.
  */
 app.get('/u/:shortURL', (req, res) => {
     const shortURL = req.params.shortURL;
+    let id = req.session.user_id;
 
     if (!urlDatabase[shortURL]) {
         return res.status(404).send('Page not found');
+    } else if (!id) {
+        id = generateRandomString();
     }
+
+    const visitInfo = {
+        id: id,
+        date: new Date()
+    }
+
+    urlDatabase[shortURL][history].push(visitInfo);
 
     const longURL = urlDatabase[shortURL].longURL;
     res.redirect(longURL);
