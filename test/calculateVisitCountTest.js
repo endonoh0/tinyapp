51bae54426f897b317ce20c52798d949f662abf6
Eric Ho <endonoho@gmail.com>
2020-06-14 13:42:22 -0700

    Update test to include base cases

--- test/calculateVisitCountTest.js
+++ test/calculateVisitCountTest.js
@@ -2,7 +2,30 @@ const { assert } = require('chai');
 const { calculateVisitCount } = require('../helpers.js');
 
 describe('calculateVisitCount', function () {
-    it('should return the number of total and unique visiters for a given url history', function () {
+    it('should return 1 unique and 1 total with the given url history', function () {
+        const urlHistory = {
+            history:
+                [{ visitorID: "d92s9f", date: "2020-06-13T22:40:47.315Z" }]
+        };
+
+        const result = calculateVisitCount(urlHistory);
+        const expectedOutput = { total: 1, unique: 1 };
+
+        assert.deepEqual(result, expectedOutput);
+    });
+
+    it('should return 0 unique and 0 total visits for a given url with no history', function () {
+        const urlHistory = {
+            history: []
+        };
+
+        const result = calculateVisitCount(urlHistory);
+        const expectedOutput = { total: 0, unique: 0 };
+
+        assert.deepEqual(result, expectedOutput);
+    });
+
+    it('should return 3 total and 1 unique visiters with a given url history', function () {
         const urlHistory = {
             history:
                 [{ visitorID: "d92s9f", date: "2020-06-13T22:40:47.315Z" },
@@ -16,7 +39,7 @@ describe('calculateVisitCount', function () {
         assert.deepEqual(result, expectedOutput);
     });
 
-    it('should return the number of total and unique visiters for a given history', function () {
+    it('should return 5 total and 4 unique visiters with a given url history', function () {
         const urlHistory = {
             history:
                 [{ visitorID: "d92s9f", date: "2020-06-13T22:40:47.315Z" },
--- test/urlHistoryForUsersTest.js
+++ test/urlHistoryForUsersTest.js
@@ -3,6 +3,17 @@ const { urlHistoryForUsers } = require('../helpers.js');
 const { urlDatabase } = require('../database');
 
 describe('urlHistoryForUsers', function () {
+    it('should return an empty history array for a valid id with no history', function () {
+        const result = urlHistoryForUsers("3ksl3d", urlDatabase);
+        const expectedOutput = {
+            "kd29ds": {
+                history: []
+                }
+            }
+
+        assert.deepEqual(result, expectedOutput);
+    });
+
     it('should return a collection of URL history with a valid user id', function () {
         const result = urlHistoryForUsers("aJ48lW", urlDatabase);
         console.log(result);
