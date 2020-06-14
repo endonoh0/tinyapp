d09492c5a9703b3fa662bf0dbc3393adbb131c27
Eric Ho <endonoho@gmail.com>
2020-06-13 17:38:41 -0700

    Append HTTP to long url if it is not included in the edit

--- routes/updateUrl.js
+++ routes/updateUrl.js
@@ -8,11 +8,16 @@ const { urlDatabase } = require('../database');
 app.put('/urls/:shortURL/update', (req, res) => {
     const shortURL = req.params.shortURL;
     const id = req.session.user_id;
+    let longURL = req.body.longURL;
 
     if (!id || urlDatabase[shortURL].userID !== id) {
         res.status(403).send('You do not have permission.')
-    } else {
-        urlDatabase[shortURL].longURL = req.body.longURL;
-        res.redirect('/urls');
     }
+
+    if (!longURL.includes("http://")) {
+        longURL = `http://${longURL}`;
+    }
+
+    urlDatabase[shortURL].longURL = longURL;
+    res.redirect('/urls');
 });
